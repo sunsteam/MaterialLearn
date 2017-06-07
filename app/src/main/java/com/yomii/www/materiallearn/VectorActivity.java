433c0a01fd06d743a7ceb7dda0db70c83ef39b10
Y.X.Sun <sunsteam@163.com>
2017-06-07 10:42:19 +0800

    增加vector动画

--- app/src/main/java/com/yomii/www/materiallearn/VectorActivity.java
+++ app/src/main/java/com/yomii/www/materiallearn/VectorActivity.java
@@ -1,8 +1,11 @@
 package com.yomii.www.materiallearn;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.ImageView;
 
 /**
  * Created by Yomii on 2017/2/7.
@@ -16,6 +19,12 @@ public class VectorActivity extends BaseActivity {
         setContentView(R.layout.activity_vector);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             setupEnterAnim();
+        }
+
+        ImageView imageView = (ImageView) findViewById(R.id.anim_image);
+        Drawable drawable = imageView.getDrawable();
+        if (drawable instanceof Animatable){
+            ((Animatable) drawable).start();
         }
     }
 }
